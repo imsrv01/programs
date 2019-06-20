03200a3f8c7e8a1eb67bacc0b61ebc1d2e5ef6bb
imsrv01 <39862925+imsrv01@users.noreply.github.com>
2019-06-20 16:07:58 -0500

    Create python_samples_1.py

---
+++ python_samples_1.py
@@ -0,0 +1,18 @@
+
+#Length
+str="hello"
+print("Length of str - ", len(str))
+
+
+#Range range(start, stop, step) - all are integers..
+for i in range(6):
+  print(i)
+  
+for i in range(1,6):
+  print(i)
+  
+for i in range(1,6,2):
+  print(i)
+  
+  
+  

987b4f7bb2ae5672a8025f45e6791c7a2b1c53e5
imsrv01 <39862925+imsrv01@users.noreply.github.com>
2019-06-28 11:37:38 -0500

    Update python_samples_1.py

--- python_samples_1.py
+++ python_samples_1.py
@@ -1,8 +1,10 @@
 
-#Length
+#### Length
 str="hello"
 print("Length of str - ", len(str))
 
+arr = [1,2,3,4,5]
+print("Length of array - ", len(arr))
 
 #Range range(start, stop, step) - all are integers..
 for i in range(6):
@@ -14,5 +16,18 @@ for i in range(1,6):
 for i in range(1,6,2):
   print(i)
   
-  
-  
+#### PARSE
+arr = [1,2,3,4,5]
+print("Reversed array - ", arr[::-1])
+print("first 2 elements of array - ", arr[0:2:])
+
+str = "hello"
+print("Reversed string - ", str[::-1])
+print("substring - ", str[1:3:])
+
+#### REVERSE
+o	testList.reverse() # inplace reverse, no copy is created, do not return new list.
+o	testList[::-1]   # slicing method
+o	list(reversed(testList)) # returns iterator and converted to list
+
+

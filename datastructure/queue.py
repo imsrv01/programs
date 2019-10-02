cbac3e7ac19228c5fd0dcf8d6e7380bdf37e9fb1
imsrv01 <39862925+imsrv01@users.noreply.github.com>
2019-10-01 20:03:31 -0500

    Create queue.py

---
+++ datastructure/queue.py
@@ -0,0 +1,38 @@
+''' 
+Appends and pops from the end of list are fast, doing inserts or pops from the 
+beginning of a list is slow (because all of the other elements have to be shifted by one). 
+Use collections.deque (Double Ended Queue)
+'''
+from collections import deque
+class Queue:
+    def __init__(self, items=[]):
+        self.items = deque(items)
+        #self.items = items
+    
+    def size(self):
+        return len(self.items)
+    
+    def isEmpty(self):
+        return self.size() == 0
+    
+    def enqueue(self, data):
+        self.items.append(data)
+    
+    def dequeue(self):
+        return self.items.popleft()
+        #return self.items.pop(0)
+    
+    
+q = Queue()
+print(q.size())
+print(q.isEmpty())
+q.enqueue(4)
+q.enqueue(7)
+q.enqueue(9)
+
+print(q.items)
+
+print(q.dequeue())
+print(q.items)
+
+

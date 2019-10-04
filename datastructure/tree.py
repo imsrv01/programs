59096ba6baf9b191cc5f1f9ae9c2699a2d410e8f
imsrv01 <39862925+imsrv01@users.noreply.github.com>
2019-10-03 21:16:44 -0500

    Update tree.py

--- datastructure/tree.py
+++ datastructure/tree.py
@@ -21,13 +21,43 @@ def height(root):
     
     return (max + 1)
     
+def search(root, data):
+    if root == None:
+        return False
+    
+    if root.data == data:
+        return True
+    elif data > root.data:
+        return search(root.right, data)
+    else:
+        return search(root.left, data)
+        
+def insert(root,data):
+    if root == None:
+        print("empty tree")
+    
+    if data > root.data:
+        if root.right == None:
+            root.right = Node(data)
+        else:
+            insert(root.right, data)
+    else:
+        if root.left == None:
+            root.left = Node(data)
+        else:
+            insert(root.left, data)
+    
 root = Node("25")
-root.left = Node("45")
-root.right = Node("55")
+root.left = Node("15")
+root.right = Node("35")
 root.right.right = Node("75")
 root.right.right.right = Node("81")
 #root.right.right.right.right = Node("59")
-root.left.left = Node("17")
-root.left.left.left = Node("59")
+root.left.left = Node("10")
+root.left.left.left = Node("5")
 
+checkifpresent = "41"
 print('Height of tree is {}'.format(height(root)))
+print("{} present ? - {}".format(checkifpresent, search(root, checkifpresent)))
+
+insert(root, "66")

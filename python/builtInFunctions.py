e83f9672e75ba6a1960fd9b421160f195afaf82d
imsrv01 <39862925+imsrv01@users.noreply.github.com>
2019-10-06 16:03:29 -0500

    Update builtInFunctions.py

--- python/builtInFunctions.py
+++ python/builtInFunctions.py
@@ -37,3 +37,22 @@ print(dog1 == dog2)
 i = input('Enter you name')
 print('your name {}'.format(i))
 
+# The ord() method returns an integer representing Unicode code point for the given Unicode character.
+# Input - c - character string of length 1 whose Unicode code point is to be found
+# Output - 99
+print(ord('c'))
+
+# Inverse of above
+# The chr() method returns a character (a string) from an integer (represents unicode code point of the character).
+# The valid range of the integer is from 0 through 1,114,111.
+# Output - c
+print(chr(99))
+
+# Returns type/class of object
+# Output - <class 'int'>
+a=40
+print(type(a))
+
+# The bin() method converts and returns the binary equivalent string of a given integer. If the parameter isn't an integer, it has to implement __index__() method to return an integer.
+# Output - 0b1000
+print(bin(8))

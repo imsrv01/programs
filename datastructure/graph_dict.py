6ddfa3d02880b7f68161671ede7eb17869cfab8e
imsrv01 <39862925+imsrv01@users.noreply.github.com>
2019-10-11 17:28:08 -0500

    Update graph_dict.py

--- datastructure/graph_dict.py
+++ datastructure/graph_dict.py
@@ -38,14 +38,54 @@ class Graph:
         for edge in self.__generate_edges():
             res += str(edge) + " "
         return res            
-
+    
+    # Simple Path - Path with no repeated vertices
+    # Path - A path in an undirected graph is a sequence of vertices P = ( v1, v2, ..., vn ) 
+    #       such that vi is adjacent to v{i+1} for 1 ≤ i < n. Such a path P is called a path of length n from v1 to vn.
+    def find_path(self, start_vertex, end_vertex, path=None):
+        
+        graph = self.graph
+        if path == None:
+            path = []
+        path = path + [start_vertex]
+        if start_vertex == end_vertex:
+            return path
+        if start_vertex not in graph:
+            return None
+        
+        for vertex in graph[start_vertex]:
+            if vertex not in path:
+                final_path = self.find_path(vertex, end_vertex, path)
+                if final_path:
+                    return final_path
+        return None
+    
+    def find_path_all(self, start_vertex, end_vertex, path=None):
+        
+        graph = self.graph
+        if path == None:
+            path = []
+        path = path + [start_vertex]
+        if start_vertex == end_vertex:
+            return [path]
+        if start_vertex not in graph:
+            return []
+        paths=[]
+        for vertex in graph[start_vertex]:
+            if vertex not in path:
+                final_path = self.find_path_all(vertex, end_vertex, path)
+                
+                for p in final_path:
+                    paths.append(p)
+        return paths
+        
 if __name__ == "__main__":
-    g = { "a" : ["d"],
+    g = { "a" : ["d",'f'],
           "b" : ["c"],
           "c" : ["b", "c", "d", "e"],
-          "d" : ["a", "c"],
+          "d" : ["a", "c",'f'],
           "e" : ["c"],
-          "f" : []
+          "f" : ['d','a']
             }
     graph = Graph(g)
     print('Vertices - ', graph.vertices())
@@ -56,5 +96,10 @@ if __name__ == "__main__":
     graph.add_edge('a', 'z')
     print('Edges - ', graph.edges())
     print(graph)
+    print('Find path - A to B ', graph.find_path('a', 'b'))
+    print('Find path - A to F ', graph.find_path('a', 'f'))
+    print('Find path - C to C ', graph.find_path('c', 'c'))
+    print('Find path - B to D', graph.find_path('b', 'd'))
+    print('Find path all - A to B', graph.find_path_all('a', 'b'))
     
     

23a5baa00df9f306d7a737b2b96988eed7ee3926
imsrv01 <39862925+imsrv01@users.noreply.github.com>
2020-02-22 14:39:31 -0600

    Update binarysearch_recursive.py

--- algorithm/searching/binarysearch_recursive.py
+++ algorithm/searching/binarysearch_recursive.py
@@ -1,18 +1,22 @@
 def binarySearch(nums, low, high, target):
+    # only if high is greaterthan or equal to low
     if high >= low: 
         # find the pivot
         middle = int((low + high)//2)
-            
+        
+        # check if pivot element is equal to target element, return pivot index if tue    
         if nums[middle] == target:
             return middle
+        # if pivot is lesser than target, call binary search on right side of array
+        # low = middle+1 
         if nums[middle] < target:
+            # Accept the returned index
             index = binarySearch(nums,middle+1, high,target)
         else:
+            # if pivot is greater than target, call binary search on left side of array
+            # high = middle-1
             index = binarySearch(nums,low, middle-1, target)
         return index
     else:
+        # return -1 if not found
         return -1
-
-if __name__ == '__main__':
-    nums = [1,4,6,7,8,9]
-    print(binarySearch(nums, 0, len(nums)-1, 10))
